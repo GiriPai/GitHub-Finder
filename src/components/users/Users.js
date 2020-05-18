c3f0ab19c35c6cd7d87a1649fcf926789f9bfeb6
Giri-Pai <giri@gaindecktechnologies.com>
2020-05-19 02:05:51 +0530

    Integrating Context API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
 
-import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Users = ({ users, loading }) => {
-  if (loading) {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  if (githubContext.loading) {
     return <Spinner />;
   } else {
     return (
       <div style={userStyle}>
-        {users.map((user) => (
+        {githubContext.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
 };
 
 const userStyle = {
